c6b55bf6825fe886e1f8f1b27a9db45a5aa75266
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-04-10 02:40:13 -0400

    Initial

---
+++ lua/includes/modules/playerload.lua
@@ -0,0 +1,9 @@
+hook.Add( "PlayerInitialSpawn","FullLoadSetup", function( spawnedPly )
+    hook.Add( "SetupMove", spawnedPly, function( self, ply, _, cmd )
+        if self ~= ply then return end
+        if cmd:IsForced() then return end
+
+        hook.Run( "PlayerFullLoad", self )
+        hook.Remove( "SetupMove", self )
+    end)
+end)
