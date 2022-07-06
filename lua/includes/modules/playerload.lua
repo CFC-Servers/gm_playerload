efb8116b0078c1898453b67b3eec16ad9b2749b2
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-07-05 19:27:36 -0700

    Change hook name to prevent conflicts

--- lua/includes/modules/playerload.lua
+++ lua/includes/modules/playerload.lua
@@ -1,4 +1,4 @@
-hook.Add( "PlayerInitialSpawn","FullLoadSetup", function( spawnedPly )
+hook.Add( "PlayerInitialSpawn","GM_FullLoadSetup", function( spawnedPly )
     hook.Add( "SetupMove", spawnedPly, function( self, ply, _, cmd )
         if self ~= ply then return end
         if cmd:IsForced() then return end
